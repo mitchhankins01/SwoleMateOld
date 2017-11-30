6d055b09c93dff83f0b2c6a465a907b5afb497c4
Mitch Hankins <mitchhankins92@gmail.com>
2017-11-29 20:15:30 -0600

    fixed android build settings

--- android/app/src/main/java/com/devpeaks/swolemate/MainApplication.java
+++ android/app/src/main/java/com/devpeaks/swolemate/MainApplication.java
@@ -3,7 +3,6 @@ package com.devpeaks.swolemate;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -39,7 +38,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
-            new RNSpinkitPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
         new RNFirebasePackage(),
@@ -56,7 +54,7 @@ public class MainApplication extends Application implements ReactApplication {
         new RNFirebaseStoragePackage()
       );
     }
-    
+
     @Override
     protected String getJSMainModuleName() {
       return "index";

944d6e945ff8b930fca8c382c8c8aae82a193b45
Mitch Hankins <mitchhankins92@gmail.com>
2017-12-04 15:22:10 -0600

    added empty card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -45,9 +45,34 @@ class Card extends Component {
     }
   }
 
+  renderEmptyCard(styles, title) {
+    return (
+      <View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <Entypo style={styles.cardTitle} name={'add-to-list'} />
+          <Text style={styles.cardTitle}>
+            `Use the buttons below to add a new ${title}`
+          </Text>
+        </View>
+        <View style={styles.cardDivider} />
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+          <Entypo
+            size={25}
+            name={'edit'}
+            style={styles.cardIcon}
+            underlayColor={'transparent'}
+            onPress={() => this.showPopover}
+          />
+        </View>
+      </View>
+    );
+  }
+
   render() {
-    const { theme, screenIndex, item, subtitle, icon, onPress } = this.props;
+    const { theme, screenIndex, empty, title, item, subtitle, icon, onPress } = this.props;
     const styles = themeStyles[theme];
+
+    if (empty) return this.renderEmptyCard(styles, title);
 
     return (
       <TouchableOpacity key={item.name} style={styles.cardContainer} onPress={onPress} >

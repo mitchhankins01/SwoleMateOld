91d0340b0fbc2cf4632980598edb5785ed2da947
Mitch Hankins <mitchhankins92@gmail.com>
2018-01-08 16:41:06 -0700

    picker => func comp

--- src/components/Picker/Picker.js
+++ src/components/Picker/Picker.js
@@ -1,77 +1,66 @@
 import _ from 'lodash';
-import { Platform, Picker as RNPicker } from 'react-native';
-import React, { Component } from 'react';
+import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { Platform, Picker as RNPicker } from 'react-native';
 import { WheelPicker } from 'react-native-wheel-picker-android';
 
 import themeStyles from './styles';
 
-@inject('workoutStore', 'userStore') @observer
-class Picker extends Component {
-  constructor(props) {
-    super(props);
-    this.repsArray = _.range(0, 101, 1);
-    this.weightArray = _.range(0, 505, 5);
+const getNumbers = (type, array) => {
+  switch (type) {
+    default: return null;
+    case 'reps': return array.map(number => number.toString());
+    case 'weight': return array.map(number => number.toString());
   }
+};
 
-  onChangeInput(type, number) {
-    switch (type) {
-      default: return;
-      case 'reps': return this.props.setReps(number); //this.setState({ reps: number });
-      case 'weight': return this.props.setWeight(number); // this.setState({ weight: number });
-    }
+const onChangeInput = (type, number, setReps, setWeight) => {
+  switch (type) {
+    default: return;
+    case 'reps': return setReps(number); //this.setState({ reps: number });
+    case 'weight': return setWeight(number); // this.setState({ weight: number });
   }
+};
 
-  getNumbers(type) {
-    switch (type) {
-      default: return null;
-      case 'reps': return this.repsArray.map(number => number.toString());
-      case 'weight': return this.weightArray.map(number => number.toString());
-    }
+export default inject('userStore')(observer((props) => {
+  const styles = themeStyles[props.userStore.selected];
+  const { type, weight, reps, setReps, setWeight } = props;
+
+  let array = _.range(0, 505, 5);
+  if (type === 'reps') array = _.range(0, 101, 1);
+
+  switch (Platform.OS) {
+    case 'android':
+      return (
+        <WheelPicker
+          isCurved
+          renderIndicator
+          itemTextSize={40}
+          itemTextColor='#EDF0F1'
+          data={getNumbers(type, array)}
+          itemTextFontFamily='Exo-Medium'
+          selectedItemTextColor='#EDF0F1'
+          indicatorColor={styles.$primaryColor}
+          style={{ height: 200, marginTop: 10, width: '100%' }}
+          onItemSelected={event => onChangeInput(type, event.data, setReps, setWeight)}
+          selectedItemPosition={
+            type === 'weight' ? Number(weight / 5) : Number(reps)
+          }
+        />
+      );
+    default:
+      return (
+        <RNPicker
+          onValueChange={number => onChangeInput(type, number, setReps, setWeight)}
+          selectedValue={type === 'weight' ? weight.toString() : reps.toString()}
+          itemStyle={{ textAlign: 'center', color: '#EDF0F1', fontFamily: 'Exo-Medium', fontSize: 16 }}
+        >
+          {array.map(number => {
+            return (
+              <RNPicker.Item key={number} label={number.toString()} value={number.toString()} />
+            );
+          })}
+        </RNPicker>
+      );
   }
-
-  render() {
-    const { type, weight, reps } = this.props;
-    const styles = themeStyles[this.props.userStore.selected];
-
-    let array = this.weightArray;
-    if (type === 'reps') array = this.repsArray;
-
-    switch (Platform.OS) {
-      case 'android':
-        return (
-          <WheelPicker
-            isCurved
-            renderIndicator
-            itemTextSize={40}
-            itemTextColor='#EDF0F1'
-            data={this.getNumbers(type)}
-            itemTextFontFamily='Exo-Medium'
-            selectedItemTextColor='#EDF0F1'
-            indicatorColor={styles.$primaryColor}
-            style={{ height: 200, marginTop: 10, width: '100%' }}
-            onItemSelected={event => this.onChangeInput(type, event.data)}
-            selectedItemPosition={
-              type === 'weight' ? Number(weight / 5) : Number(reps)
-            }
-          />
-        );
-      default:
-        return (
-          <RNPicker
-            onValueChange={number => this.onChangeInput(type, number)}
-            selectedValue={type === 'weight' ? weight.toString() : reps.toString()}
-            itemStyle={{ textAlign: 'center', color: '#EDF0F1', fontFamily: 'Exo-Medium', fontSize: 16 }}
-          >
-            {array.map(number => {
-              return (
-                <RNPicker.Item key={number} label={number.toString()} value={number.toString()} />
-              );
-            })}
-          </RNPicker>
-        );
-    }
-  }
-}
-
-export default Picker;
+}));

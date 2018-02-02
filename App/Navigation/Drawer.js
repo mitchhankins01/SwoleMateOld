7426b3cd1c370ac026b0df84c06f89394d9cc729
Mitch Hankins <mitchhankins92@gmail.com>
2018-02-01 18:09:07 -0700

    implemented theming drawer

--- App/Navigation/Drawer.js
+++ App/Navigation/Drawer.js
@@ -1,33 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Text } from 'react-native';
 import { DrawerItems, SafeAreaView } from 'react-navigation';
 
-import { Colors, Fonts } from '../Themes';
+import { ThemeSelector, Fonts } from '../Themes';
 
-const Drawer = props => (
-  <SafeAreaView
-    forceInset={{ top: 'always', horizontal: 'never' }}
-    style={{ flex: 1, backgroundColor: Colors.tertiaryColor }}
-  >
-    <Text
-      style={{
-        fontSize: 35,
-        marginVertical: 20,
-        color: Colors.text,
-        alignSelf: 'center',
-        fontFamily: Fonts.type.bold,
-        backgroundColor: 'transparent',
-      }}
+const Drawer = (props) => {
+  const Colors = ThemeSelector(props.theme);
+  return (
+    <SafeAreaView
+      forceInset={{ top: 'always', horizontal: 'never' }}
+      style={{ flex: 1, backgroundColor: Colors.tertiaryColor }}
     >
-      SwoleMate
-    </Text>
-    <DrawerItems
-      {...props}
-      inactiveTintColor="#BEC3C7"
-      activeTintColor={Colors.primaryColor}
-      labelStyle={{ fontFamily: 'Exo-Bold', fontSize: 16 }}
-    />
-  </SafeAreaView>
-);
+      <Text
+        style={{
+          fontSize: 35,
+          marginVertical: 20,
+          color: Colors.text,
+          alignSelf: 'center',
+          fontFamily: Fonts.type.bold,
+          backgroundColor: 'transparent',
+        }}
+      >
+        SwoleMate
+      </Text>
+      <DrawerItems
+        {...props}
+        inactiveTintColor="#BEC3C7"
+        activeTintColor={Colors.primaryColor}
+        labelStyle={{ fontFamily: 'Exo-Bold', fontSize: 16 }}
+      />
+    </SafeAreaView>
+  );
+};
 
-export default Drawer;
+export default connect(({ auth }) => auth)(Drawer);
